57ad32a96e7e941ce6dbe08c616ab39cd1a5809a
Arthur Schellhase <snupiking1@gmail.com>
2024-05-06 22:15:33 +0200

    - Added some Files and Images - Made the Website to be installable as a PWA - Changed some other minor things

---
+++ src/service-worker.js
@@ -0,0 +1,18 @@
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open('my-cache').then(cache => {
+            return cache.addAll([
+                '/',
+                '/icon.png'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
